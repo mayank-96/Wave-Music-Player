87b531b131116d82e0e34b6c37a8a7f744681ec8
mayank-96 <pagarmayank07@gmail.com>
2021-09-18 18:54:41 +0530

    Added: Song & Player Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+// Importing Components
+import Song from "./components/Song";
+import Player from "./components/Player";
+
+// Import Styles
+import "./styles/app.scss";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h2>Song Name</h2>
+      <h2>Artist</h2>
+    </div>
+  );
+};
+
+export default Song;
