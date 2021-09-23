adb5b3b80524dc3b08226a1729a9d747d9e1df9b
mayank-96 <pagarmayank07@gmail.com>
2021-09-23 19:00:39 +0530

    Added: Favicon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../Logo.png";
 
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
-      <h1>Waves</h1>
+      <div className="logo">
+        {/* <img src={Logo} alt="Logo"></img> */}
+        <span className="gap"></span>
+        <h1>Waves</h1>
+      </div>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Library
+        <span className="gap"></span>
         <FontAwesomeIcon icon={faMusic} />
       </button>
     </nav>
